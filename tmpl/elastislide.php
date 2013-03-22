99ec7ff4681b24d82931cc3dc97f586def5bb1ae
gnanakeethan <gnanakeethan@gmail.com>
2013-03-22 15:58:23 +0530

    vertical,boxed,fixed bar has been added to select image option

--- tmpl/elastislide.php
+++ tmpl/elastislide.php
@@ -14,7 +14,7 @@ $doc->addStyleSheet('modules/mod_gn_gallery/assets/css/elastislide/elastislide.c
 $doc->addStyleSheet('modules/mod_gn_gallery/assets/css/elastislide/custom.css');
 $doc->addStyleSheet('modules/mod_gn_gallery/assets/css/elastislide/setup.css');
 $doc->addScript('modules/mod_gn_gallery/assets/js/elastislide/modernizr.custom.17475.js');
-$style='div#top{
+$style = 'div#top{
 	background: #ffffff !important;
 	border-color:#ffffff !important;
 	
@@ -70,7 +70,6 @@ switch ($this->config['elastislide']['mode']) {
 				echo '</div>';
 			}
 
-			//vardump ($this->config['elastislide']['directory']);
 			/**
 			 * ****************************
 			 * Directory to Gallery End
@@ -79,6 +78,14 @@ switch ($this->config['elastislide']['mode']) {
 		}
 		break;
 	case 'select' : {
+			if ($this->config['elastislide']['type'] == 'vertical') {
+				echo '
+				<div class="column">';
+			} elseif ($this->config['elastislide']['type'] == 'boxed') {
+				echo '<div class="gallery">';
+			} elseif ($this->config['elastislide']['type'] == 'fixed_bottom') {
+				echo '<div class="fixed-bar">';
+			}
 			echo "<ul id='carousel' class='elastislide-list'>";
 			foreach ($this->config['elastislide']['selected'] as $selected) {
 				if (!$selected) {
@@ -89,10 +96,18 @@ switch ($this->config['elastislide']['mode']) {
 				}
 			}
 			echo "</ul>";
+			if ($this->config['elastislide']['type'] == 'boxed') {
+				echo '<div class="image-preview">
+						<img id="preview" src="' . $str . '" />
+					</div>';
+			}
+			if ($this->config['elastislide']['type'] == 'vertical' || $this->config['elastislide']['type'] == 'boxed' || $this->config['elastislide']['type'] == 'fixed_bottom') {
+				echo '</div>';
+			}
 		}
 		break;
 	default : {
-			echo 'Default Case';
+			echo 'We Are Unable to Determine what you have choosen';
 		}
 }
 ?>
@@ -106,13 +121,13 @@ switch ($this->config['elastislide']['mode']) {
 if ($this->config['elastislide']['type'] == 'vertical' || $this->config['elastislide']['type'] == 'fixed_bottom' || $this->config['elastislide']['type'] == 'horizontal') {
 	echo "$('#carousel').elastislide({\n";
 	echo 'orientation:\'' . $this->config['elastislide']['type'] . '\',' . "\n";
-	echo 'minItems :'. $this->config['elastislide']['min_items'].','. "\n";
-	echo 'speed :'.$this->config['elastislide']['timing'].','. "\n";
+	echo 'minItems :' . $this->config['elastislide']['min_items'] . ',' . "\n";
+	echo 'speed :' . $this->config['elastislide']['timing'] . ',' . "\n";
 	echo 'onClick : function( el, position, evt ) { return false; },
 		onReady : function() { return false; },
 		onBeforeSlide : function() { return false; },
 		onAfterSlide : function() { return false; }';
-	echo "});". "\n";
+	echo "});" . "\n";
 }
 if ($this->config['elastislide']['type'] == 'boxed') {
 	echo "var current = 0,
@@ -121,7 +136,7 @@ if ($this->config['elastislide']['type'] == 'boxed') {
 				\$carouselItems = \$carouselEl.children(),
 				carousel = \$carouselEl.elastislide( {
 					current : current,
-					minItems :".$this->config['elastislide']['min_items'].",
+					minItems :" . $this->config['elastislide']['min_items'] . ",
 					onClick : function( el, pos, evt ) {
 
 						changeImage( el, pos );
