e881ecf802cc65fe9ec8ce0dee2d17d9e2df2791
gnanakeethan <gnanakeethan@gmail.com>
2013-03-24 10:56:56 +0530

    changelog edited

--- docs/CHANGELOG.php
+++ docs/CHANGELOG.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Module Gnanam Gallery - Multi-Purpose Carousel Module by Gnanakeethan
  * 
- * @author     Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
- * @copyright  (c) 2013 Gnanakeethan Balasubramaniam
- * @license    GNU/GPL v3 or Later.
+ * @package	  ModuleGNGallery
+ * @author    Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
+ * @copyright (c) 2013 Gnanakeethan Balasubramaniam
+ * @license   GNU/GPL v3 or Later.
  */
 // no direct access
 defined('_JEXEC') or die('Restricted access');
@@ -17,5 +19,29 @@ defined('_JEXEC') or die('Restricted access');
  * @license    GNU/GPL v3 or Later.
  */
 Class ChangeLog {
-	
+	/**
+	 * @version 1.0.0 Beta 2<br/>
+	 * Added Z-Index -  A Z-Index (CSS) Based Flap Gallery.<br/>
+	 * <b>New Features</b>
+	 * <ul>
+	 * <li>Flap Gallery</li>
+	 * </ul>
+	 * 
+	 * 
+	 * @version 1.0.0 Beta 1<br/>
+	 * Added Elastislide - A Carousel Display.<br/>
+	 *<b>New Features</b>
+	 * <ul>
+	 * <li>Completely Responsive</li>
+	 * <li>Directory to Carousel</li>
+	 * <li>Horizontal Carousel</li>
+	 * <li>Vertical Carousel</li>
+	 * <li>Boxed - Preview Carousel</li>
+	 * <li>FixedBar - Fixed to Bottom of Browser Screen</li>
+	 * </ul>
+	 * 
+	 */
+	function ChangeLog() {
+		
+	}
 }
