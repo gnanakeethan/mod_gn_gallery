4e01de470b66787241090ebcf4723974219b5403
gnanakeethan <gnanakeethan@gmail.com>
2013-03-24 07:18:25 +0530

    adding changelog

---
+++ docs/CHANGELOG.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Module Gnanam Gallery - Multi-Purpose Carousel Module by Gnanakeethan
+ * 
+ * @author     Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
+ * @copyright  (c) 2013 Gnanakeethan Balasubramaniam
+ * @license    GNU/GPL v3 or Later.
+ */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * ChangeLog for Gnanam Gallery - Multi-Putpose Gallery Module by Gnanakeethan.
+ * 
+ * @author     Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
+ * @copyright  (c) 2013 Gnanakeethan Balasubramaniam
+ * @license    GNU/GPL v3 or Later.
+ */
+Class ChangeLog {
+	
+}
